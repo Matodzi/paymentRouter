4d2ebee229dfa8fcf42cdaa476162b5e24d0639a
Matodzi <nthangeni.matodzi@gmail.com>
2019-02-09 06:00:48 +0200

    Initial commitoj java file

---
+++ src/main/java/za/co/paymentRouter/PaymentRouterApplication.java
@@ -0,0 +1,19 @@
+package za.co.paymentRouter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
+
+//@ComponentScan
+//@EntityScan
+@EnableAutoConfiguration
+@SpringBootApplication
+public class PaymentRouterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentRouterApplication.class, args);
+    }
+}
